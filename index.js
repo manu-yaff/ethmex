233b4e248da8c6b4011dcae418099ed6234a648e
Royer Donnet Arenas Camacho <A01209400@itesm.mx>
2022-08-20 22:22:01 -0500

    Feat: balance endpoint

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 import dot from 'dotenv';
 import Web3 from 'web3';
 import Contract from 'web3-eth-contract';
@@ -10,19 +10,18 @@ const app = express();
 const port = process.env.PORT;
 const polygonInfuraUrl = process.env.POLYGON_INFURA_URL;
 const walletPrivateKey = process.env.WALLET_PRIVATE_KEY;
+const underlyingdecimals = process.env.UNDERLYING_DECIMALS;
+console.log(polygonInfuraUrl,walletPrivateKey)
 
 const web3 = new Web3(new Web3.providers.HttpProvider(polygonInfuraUrl));
 
-const setUp = () => {
-	Contract.setProvider(polygonInfuraUrl);
-	const contract = new Contract(
-		beerTokenABI,
-		'0x25A580558292aAeD2043bD3BAef0f43A6e2a49c2'
-	);
-	web3.eth.accounts.wallet.add(walletPrivateKey);
-};
+let contract = new Contract(
+	beerTokenABI,
+	'0x25A580558292aAeD2043bD3BAef0f43A6e2a49c2'
+);
 
-setUp();
+contract.setProvider(polygonInfuraUrl);
+web3.eth.accounts.wallet.add(walletPrivateKey);
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
@@ -32,3 +31,11 @@ app.get('/create-wallet', () => {
 	let newWallet = web3.eth.accounts.create();
 	// console.log(newWallet);
 });
+
+app.get('/balance',async (req,res) => {
+	let walletAddress = req.query.walletAddress
+	let balance = await contract.methods.balanceOf(walletAddress).call();
+	return res.json({balance:balance});
+})
+
+
