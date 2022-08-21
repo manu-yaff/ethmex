c24faf373cc8a899758f6c6b6a8419ee578a8668
Royer Donnet Arenas Camacho <A01209400@itesm.mx>
2022-08-21 08:18:06 -0500

    hotfix: change contract abi to retrive actial balance

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ import Web3 from "web3";
 import bodyParser from "body-parser";
 import Contract from "web3-eth-contract";
 import { beerTokenABI } from "./ABI.js";
+import { maticTokenABI } from "./maticABI.js";
 import cors from "cors";
 
 dot.config();
 
 const app = express();
 app.use(bodyParser.json());
-app.options('*', cors());
+//app.options('*', cors());
+app.use(cors({origin: '*'}));
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  });
 const port = process.env.PORT;
 const polygonInfuraUrl = process.env.POLYGON_INFURA_URL;
 const walletPrivateKey = process.env.WALLET_PRIVATE_KEY;
@@ -24,13 +31,25 @@ let contract = new Contract(
   "0x25A580558292aAeD2043bD3BAef0f43A6e2a49c2"
 );
 
+
+let contract2 = new Contract(
+	maticTokenABI,
+	"0x0000000000000000000000000000000000001010"
+  );
+
+
+
 let corsOptions = {
 	origin:'*',
+	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 	optionsSuccessStatus:200
 }
 
 contract.setProvider(polygonInfuraUrl);
+contract2.setProvider(polygonInfuraUrl);
 web3.eth.accounts.wallet.add(walletPrivateKey);
+
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
@@ -55,21 +74,24 @@ app.get("/balance", async (req, res) => {
   }
   let walletAddress = req.query.walletAddress;
   try {
-    let balance = await contract.methods.balanceOf(walletAddress).call();
-    return res.json({ balance: balance });
+    let balance = await contract2.methods.balanceOf(walletAddress).call();
+	balance = balance.toString();
+    console.log("balance", balance[0]);
+	return res.json({ balance: balance[0] });
   } catch (err) {
     return res.json({ status: 500, message: err.message });
   }
 });
 
 app.post("/deposit", async (req, res) => {
+	console.log('deposit');
   if (!req.body.address || !req.body.amount) {
     return res.json({
       status: 400,
       message: "Address and amount are required",
     });
   }
-  let walletAddress = req.body.walletAddress;
+  let walletAddress = req.body.address;
   let amount = req.body.amount;
   let value = 0;
 
@@ -90,7 +112,7 @@ app.post("/deposit", async (req, res) => {
           signedTx.raw || signedTx.rawTransaction
         );
         sentTx.on("receipt", (receipt) => {
-          //console.log(receipt);
+          console.log(receipt);
           return res.json({ status: 200, receipt: receipt });
         });
         sentTx.on("error", (err) => {
